d257dae58d7a024c17e0cd9a1495c85da00dea55
Chris Nance <christopher.nance@icloud.com>
2022-02-20 10:48:56 -0600

    Initial commit of Text Analyzer

--- Text Analyzer.py
+++ Text Analyzer.py
@@ -1,112 +1,110 @@
 # Chris Nance
-# Prof. Klmup
-# Programming Fundamentals
-# 2/6/2022
-
-
-## MODULES/LIBRARIES
-from random import randint
-
-
-print(r'''
-*******************************************************
-                  Rock Paper Scissors                
-*******************************************************
-
-In this classic game, you will play a number of
-seven-game series of rock-paper-scissors against
-the computer. I will show you who won each series,
-and I will show you how many series you won and
-lost at the end.
-''')
+# Prof. Klump
+# Feb. 20th, 2022
+# Text Analyzer
 
 
 ## VARIABLES
-series_to_play = int(input("Enter the amount of series you want to play: "))
-user_score = 0
-cpu_score = 0
-series_ties = 0
-round_ties = 0
-user_series_won = 0
-cpu_series_won = 0
-user_action = ''
-cpu_action = ''
+input_validated = False
+input_file = ''
+user_option = 0
 
 
-## FUNCTION: Rock, Paper, Scissors LOGIC
-def rpsLogic(user_input, cpu_input): # Function handles logic for the game. returns 0 if the pc wins, 1 if the human wins, and 2 if it's a tie.
-    if user_input == 'r' and cpu_input == 'paper': return 0 # User: Rock   VS   PC: Paper
-    if user_input == 'p' and cpu_input == 'scissors': return 0 # User: Paper  VS   PC: Scissors
-    if user_input == 's' and cpu_input == 'rock': return 0 # User: Scissors VS PC: Rock
-    if user_input == cpu_input[0]: return 2 #                User == PC (Need to parse string for the first letter for cpu_input for comparrison)
-    return 1 # PC Didn't win, and there wasn't a tie so the human must have won
+## TABLES
+menu_options_table = [1, 2, 3, 4, 5, 6]
+vowels = ['a', 'e', 'i', 'o', 'u']
 
 
-for series in range(1, series_to_play+1): # Iterate through all the rounds the user wanted to play, series denotes the series number; loop starts at 1 not 0 for output printing purposes.
-    print("\nSeries:", series)
-
-    ## COUNTER RESET
-    user_score = 0
-    cpu_score = 0
-    round_ties = 0
-    round_num = 0 # Round number counter.
-
-    while round_num < 7: # Iterate through all 7 rounds of Rock, Paper, Scissors.
-        round_num += 1
-
-        ## INPUT & VALIDATION
-        user_action = input("Enter r for rock, p for paper or s for scissors: ")
-        while user_action.lower() != "r" and user_action.lower() != "p" and user_action.lower() != "s":  # Check input against lower()
-            print("Oops! You need to choose either 'r' for rock, 'p' for paper or 's' for scissors.")
-            user_action = input("Enter r for rock, p for paper or s for scissors: ")
-
-        ## CPU_INPUT GENERATION
-        # Randomy generate an integer from 1 up to and including 3 and convert it to a string for comparrison to user input. 
-        cpu_action = randint(1,3)
-        if cpu_action == 3:
-            cpu_action = 'rock'
-        elif cpu_action == 2:
-            cpu_action = 'paper'
-        elif cpu_action == 1:
-            cpu_action = 'scissors'
-        
-        ## SERIES OUTPUT (Using Function: rpsLogic())
-        if rpsLogic(user_action.lower(), cpu_action) == 0: # Call function fpsLogic() with arguments user_action as lower() and cpu_action
-            print("The CPU won the round. They threw %s." % cpu_action)
-            cpu_score += 1
-            if cpu_score == 4: break # If the cpu_score is 4 then end the series early
-        elif rpsLogic(user_action.lower(), cpu_action) == 1:
-            print("You won the round! The CPU threw %s." % cpu_action)
-            user_score += 1
-            if user_score == 4: break # If the user_score is 4 then end the series early
-        else: #  rpsLogic() returned 2 == Tie
-            print("You TIED! You both threw", cpu_action)
-            round_ties += 1
-
-    ## SERIES COMPLETION OUTPUT
-    print("Series #" + str(series), "is Over!")
-    if user_score > cpu_score:
-        print("You won the series!\nYou won", user_score, "rounds versus the CPU winning", cpu_score, "rounds and there were", round_ties, "tie(s).")
-        user_series_won += 1
-    elif cpu_score > user_score:
-        print("You lost the series!\nYou won", user_score, "rounds versus the CPU winning", cpu_score, "rounds and there were", round_ties, "tie(s).")
-        cpu_series_won += 1
-    else:
-        print("It was a tie!\nYou won", user_score, "the CPU won", cpu_score, "and there were", round_ties, "tie(s).")
-        series_ties += 1
+## FUNCTIONS
+def menu_options(): # Ask, Validate and Return the user option. Shows mneu too.
+    print('''Here are your choices:
+    1. Count the number of characters.
+    2. Count the number of vowels.
+    3. Count the number of words.
+    4. Extract a portion of the file.
+    5. Print the characters of the file backwards
+    6. Quit
+    ''')
+    user_option = 0
+    while user_option not in menu_options_table:
+        try:
+            user_option = int(input("Select a Menu Option: "))
+            if user_option in menu_options_table: break
+            print("That is NOT a valid menu option.\n")
+        except ValueError as e:
+            print("ERROR: You need to select an integer:", e)
+    return user_option # Return the user_option AFTER validation.
 
 
-## GAME COMPLETION OUTPUT
-print("\nGame Over!")
-if user_series_won > cpu_series_won:
-    print("You WON the game! You won", user_series_won, "series versus the CPU winning", cpu_series_won, "series. There were", str(series_ties), "tie(s).")
-elif cpu_series_won > user_series_won:
-    print("You LOST the game! You won", user_series_won, "series versus the CPU winning", cpu_series_won, "series. There were", str(series_ties), "tie(s).")
-else:
-    print("The game ended in a TIE! You won", user_series_won, "series versus the CPU winning", cpu_series_won, "series. There were", str(series_ties), "tie(s).")
+## FILE INPUT VALIDATION
+while input_validated == False:
+    try:
+        input_file = input("Enter the name of the file you want to open: ")
+        output_file = open(input_file, 'r')
+        input_validated = True
+        print("Good to parse")
+    except FileNotFoundError as error:
+        print("File Not Found:", error)
+    except Exception as error:
+        print("No good:", error)
+    
 
-print('''
-*******************************************************
-                THANK YOU FOR PLAYING!                
-*******************************************************
-''')
+## MAIN PROGRAM
+while user_option != 6:
+    output_file = open(input_file, 'r')
+    
+    if user_option == 5:
+        print("Printing characters of the file in reverse!")
+        output_file_string = output_file.read() # Convert contents of file to string
+        rev_string = output_file_string[::-1] # Splice from end --> start
+        rev_string_limited = ''
+        count = 0
+        for num, char in enumerate(rev_string):
+            rev_string_limited += char # Build the new string with 70 character per line limit.
+            if num % 70 == 0: # Check if the line is ending on a multiple of 70 (means 70 characters on the line) Remainder is 0 if True
+                rev_string_limited += '\n'
+        print(rev_string_limited)
+    elif user_option == 4:
+        print("I need a bit more information before I can extract some text...")
+        output_file_string = output_file.read() # Convert contents of file to string
+        valid_indexes = False
+        while valid_indexes != True:
+            try:
+                index_1 = int(input("Please enter the index of the first character to include: "))
+                index_2 = int(input("Please enter the index of the last character to include: "))
+                valid_indexes = True
+            except ValueError as error:
+                print("ERROR: The index MUST be an integer, not a letter.\n")
+        # Validation complete, use index_1 and index_2 to splice string.
+        print(output_file_string[index_1:index_2+1])
+    elif user_option == 3:
+        print("Counting the number of words in the file...")
+        number_of_words = 0
+        for line in output_file: # Iterate lines
+            words = line.split(' ') # Create a list of the words without spaces.
+            number_of_words += len(words) # Add each line of words to the total number of words
+        print("There are", str(number_of_words), "words in the file.")
+        output_file.close()
+    elif user_option == 2:
+        print("Counting the number of vowels in the file...")
+        number_of_vowels = 0
+        for line in output_file: # Iterate per line
+            for char in line: # Iterate per character
+                if char.lower() in vowels: # Check char.lower() if it is in vowels. 
+                    number_of_vowels += 1
+        print("Thre are", str(number_of_vowels), "vowels in the file.")
+        output_file.close()
+    elif user_option == 1:
+        print("Counting the number of characters in the file...")
+        number_of_characters = 0
+        for line in output_file:
+            for char in line:
+                if char != '' and char != ' ':
+                    number_of_characters += 1
+        print("Thre are", str(number_of_characters), "charcaters in the file.")
+        output_file.close()
+    
+    output_file.close()
+    user_option = menu_options()
+
+print("Goodbye, thanks for using my program!")
