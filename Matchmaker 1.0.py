5eea5c5a234259453b04ce1633393d7899142542
Chris Nance <christopher.nance@icloud.com>
2022-03-13 09:19:15 -0500

    Match Maker requirment changes

--- Matchmaker 1.0.py
+++ Matchmaker 1.0.py
@@ -4,19 +4,12 @@
 #date            :20220313
 #version         :1.0
 #usage           :python matchmaker_nance.py
-#notes           :
+#notes           :Edit the questionAndAnswerDict Dictionary below.
 #python_version  :3.10
-#==============================================================================
+#==============================================================================#==============================================================================#==============================================================================
 
-
-
-## VARIABLES
-user_score = 0
-user_answer = 0
-total_available = 0
-
-
-## DICTIONARIES & TABLES
+# Please add, remove or modify any questions in the dictionary. The first number in each array is the correct score for the question while the second number is the weight (or how important) the question is to you.
+# The script will automatically adjust the total points available and output based on your modifications to this dictionary. 
 questionAndAnswerDict = (
 #   ['QUESTION', SCORE, WEIGHT] 
     ['iPhone is better than Android', 4, 1],
@@ -25,6 +18,17 @@ questionAndAnswerDict = (
     ['Data science is really fun', 1, 2],
     ['I like all four seasons rather than it just being hot all year', 2, 2],
 )
+
+#==============================================================================#==============================================================================#==============================================================================
+
+
+
+## VARIABLES
+user_score = 0
+user_score_nw = 0
+user_answer = 0
+total_available = 0
+total_available_nw = 0
 
 
 ## HEADER
@@ -53,13 +57,15 @@ for question_num in range(0,len(questionAndAnswerDict)): # Ask all questions in 
         print("[ERROR]: You need to enter an INTEGER from 1 to 5...") 
     
     user_score += abs(answer - user_answer) * weight # Calculate the running total of points the user has accumulated. Abs prevents negatives.
+    user_score_nw += abs(answer - user_answer)
     total_available += answer*weight # Calculate the total points available based on the dictionary of questions. Adding/Removing/Editing questions require no code change.
+    total_available_nw += answer
 
 user_score = total_available - user_score # Obtain true user score by subtracting their score from the total score available.
 
 
 ## SCORE OUTPUT/REMARKS
-print(f"\n\nMatch Percent: {user_score/total_available*100:.2f}%.\nYou scored", str(user_score), "points out of the possible", str(total_available), "available.")
+print(f"\n\nMatch Percent: {user_score/total_available*100:.2f}%.\nYou scored", str(user_score), "weighted points out of the possible", str(total_available), "available.\nYou scored", str(user_score_nw), "non-weighted points out of the possible", str(total_available_nw), "available.")
 if user_score < total_available*0.5: # <50% match
     print("Maybe we would be better off never talking again...")
 elif user_score < total_available*0.7: # <70% match
@@ -68,7 +74,6 @@ elif user_score < total_available*0.9: # <90% match
     print("This could really work out...")
 elif user_score > total_available*0.9: # >90% match
     print("Perfect!")
-print('\n'*2)
 print('''
 --------------------------------------
 --          MATCHMAKER 1.0          --
