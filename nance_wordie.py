2221c81f2ca80f0eb2b84301d4474249c5032212
Christopher Nance <christopher.nance@icloud.com>
2022-03-16 09:12:15 -0600

    Updated Wordle to meet assignment requirements.

--- nance_wordie.py
+++ nance_wordie.py
@@ -25,6 +25,25 @@ previousGuesses = [] # // with below
 
 
 ## FUNCTIONS
+def header():
+    print('''
+    ***************************************************************************
+                                    WORDIE
+    ***************************************************************************
+
+                                  DIRECTIONS:
+    Welcome to WORDIE, the game everyone is playing. Your job is to guess
+    a five-letter word. You start with a completely blank set of five letter
+    tiles. As you guess a letter that is in the word, it will appear, either
+    out of place, in which case it will be marked with a ~ symbol, or in its
+    proper place, where it will be marked with a * symbol. Letters that are
+    out of place will be marked with an X symbol. The fewer tries it takes you 
+    to get all the letters in the right place, the faster you will guess the 
+    entire word. Only guesses that are actually words in the word list will be 
+    accepted. 
+
+    Good luck!
+    ''')
 def printGameBoard():
     print("Printing game board...")
     for item in previousGuesses: print("{: >5} {: >5} {: >5} {: >5} {: >5}".format(*item)) # https://docs.python.org/2.7/library/string.html#format-specification-mini-language
@@ -37,61 +56,42 @@ for line in open(wordlist_file, 'r'):
 
 
 ## MAIN | *=good X=bad ~=wrong spot
-print('''
-***************************************************************************
-                                   WORDIE
-***************************************************************************
 
-
-Welcome to WORDIE, the game everyone is playing. Your job is to guess
-a five-letter word. You start with a completely blank set of five letter
-tiles. As you guess a letter that is in the word, it will appear, either
-out of place, in which case it will be marked with a ~ symbol, or in its
-proper place, where it will be marked with a * symbol. Letters that are
-out of place will be marked with an X symbol. The fewer tries it takes you 
-to get all the letters in the right place, the faster you will guess the 
-entire word. Only guesses that are actually words in the word list will be 
-accepted. 
-
-Good luck!
-
-''')
 while keepPlaying.lower() == 'y':
     print('\n'*20)
-    print("Playing")
+    header()
     previousGuesses.clear()
+    guesses = 0
     wordToGuess = wordlist[randint(0, len(wordlist)-1)].lower()
     while guesses < 6:
-#        print("WORD TO GUESS:", wordToGuess)
-        printGameBoard()
+        print("WORD TO GUESS:", wordToGuess, 'split:', wordToGuess.split())
+        print("You have", str(6-guesses), 'guesses left.')
         guess = input("Enter your guess: ").lower()
-        guesses += 1
         if guess in wordlist:
             # Player's Guess was a word in the list.
-            guessToAppend = []
-            guessSymbolsToAppend = []
-            for index, character in enumerate(guess):
+            guesses += 1
+            guessToAppend = [] # Package for round 1
+            guessSymbolsToAppend = [] # Package for round 1
+            for index, character in enumerate(guess): # index, character and iterate through the player's guess.
+                guessToAppend.append((character))
                 if character == wordToGuess[index]:
                     # Good character in correct spot.
-                    guessToAppend.append((character))
                     guessSymbolsToAppend.append("*")
-                elif character in wordToGuess.split():
+                elif character in [char for char in wordToGuess]: # Convert WordToGuess to a list and see if any characters guessed are in the list.
                     # Bad placement of character.
-                    guessToAppend.append((character))
                     guessSymbolsToAppend.append("~")
                 else:
                     # Letter is not in the word.
-                    guessToAppend.append((character))
                     guessSymbolsToAppend.append("x")
-            # OUTPUT SETUP
-            previousGuesses.append(guessSymbolsToAppend)
-            previousGuesses.append(guessToAppend)
+            # OUTPUT SETUP                              #         OUTPUT MODEL: 
+            previousGuesses.append(guessSymbolsToAppend)#       *   x   ~   ~   x    <-- Marker
+            previousGuesses.append(guessToAppend)       #       a   b   a   c   k    <-- Word
             previousGuesses.append(['','','','','']) # Place holder for spacing between guesses.
             
             if guess == wordToGuess: 
                 print("Congrats! You guessed the word in less than 6 tries.")
-                break
-            print("You have", str(6-guesses), 'left.')
+                break # Exit the nested while loop
+            printGameBoard() # Display our updated gameboard.
         else:
             # Player's Guess was NOT a word in the list.
             print("Sorry, that word is not in the word list. Try again. (No guesses were used)")
