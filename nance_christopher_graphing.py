f8977d55bf607b7c45292fbb36dbe0f5e8a81f42
Christopher Nance <christopher.nance@icloud.com>
2022-04-04 09:38:48 -0600

    Added function support (broken)

--- nance_christopher_graphing.py
+++ nance_christopher_graphing.py
@@ -11,9 +11,14 @@ python_version      :3.10
 #==============================================================================#
 '''
 
+import math
 import PySimpleGUI as sg
 from matplotlib import pyplot
 
+xRange = []
+yRange = []
+
+newFigNumber = 1
 
 ## GUI SETUP
 def make_window():
@@ -43,7 +48,7 @@ def make_window():
 
 
 ## FUNCTIONS
-def validateInput(values):
+def validateInput(values): #Pass values from the GUI Window.read(); returns True if validated or returns str='Reason why validation failed'
     if values['linear'] == False and values['quadratic'] == False and values['cubic'] == False and values['logarithmic'] == False and values['linearithmic'] == False: return 'Make sure you check a graph to display!'
     if values['min-xValue'] != '' and values['max-xValue'] != '':
         try:
@@ -53,17 +58,47 @@ def validateInput(values):
             return 'Validation error. Please ensure the x-value and the y-value are typed in correctly.'
     else:
         return 'You need to fill something in for the X and Y Values & Select one of the check boxes to display a graph.'
-    if values['min-xValue'] >= values['max-xValue']: return 'Min-xValue must be less than the Max-xValue'
+    if int(values['min-xValue']) >= int(values['max-xValue']): return 'Min-xValue must be less than the Max-xValue'
     return True
 
 
 
 
 ## MatPlotLib stuff
-def button_Plot():
-    print("Button: Done:: has been pressed -- plotting")
-    pyplot.plot(values['min-xValue'], values['max-xValue'])
+def button_Plot(values):
+    print("Button Done Pressed ::: plotting")
+    xRange.clear()
+    yRange.clear()
+    if values['linear'] == True:
+        for num in range(int(values['min-xValue']), int(values['max-xValue'])+1):
+            xRange.append(num)
+            yRange.append(num)
+        pyplot.plot(xRange, yRange)
+    if values['quadratic'] == True:
+        for num in range(int(values['min-xValue']), int(values['max-xValue'])+1):
+            xRange.append(num)
+            yRange.append(num**2)
+        pyplot.plot(xRange, yRange)
+    if values['cubic'] == True:
+        for num in range(int(values['min-xValue']), int(values['max-xValue'])+1):
+            xRange.append(num)
+            yRange.append(num**3)
+        pyplot.plot(xRange, yRange)
+    if values['logarithmic'] == True:
+        for num in range(int(values['min-xValue']), int(values['max-xValue'])+1):
+            xRange.append(num)
+            yRange.append(math.log(num))
+        pyplot.plot(xRange, yRange)
+    if values['linearithmic'] == True:
+        for num in range(int(values['min-xValue']), int(values['max-xValue'])+1):
+            xRange.append(num)
+            yRange.append(num*math.log(num))
+        pyplot.plot(xRange, yRange)
     
+    newFigure = pyplot.figure(newFigNumber) # Create a new figure with a random 3-digit integer ID.
+    newFigNumber += 1
+
+    pyplot.show()
 
 
 
@@ -83,7 +118,7 @@ while True:  # Event Loop
         if validateInput(values) == True:
             # Plot Graphs
             print('Plotting graphs')
-            button_Plot()
+            button_Plot(values)
         else:
             sg.popup_error('Validation Error', validateInput(values))
 
